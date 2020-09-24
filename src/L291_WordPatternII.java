6bd8d0c346efc36bda8352f5a4447dd4a0eb812d
YinuoWang-25 <yinuowang0825@gmail.com>
2020-09-23 21:32:17 -0700

    291. Word Pattern II

--- src/L291_WordPatternII.java
+++ src/L291_WordPatternII.java
@@ -1,21 +1,21 @@
-package todo;
+// 291. Word Pattern II
 
-public class WordPatternII {
-    // 291. Word Pattern II
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class L291_WordPatternII {
     public boolean wordPatternMatch(String pattern, String str) {
-        Map<Character, String> dict = new HashMap<>();
-        Set<String> used = new HashSet<>();
-        return match(pattern, str, dict, used);
+        return match(pattern, str, new HashMap<>(), new HashSet<>());
     }
 
     private boolean match(String pattern, String str, Map<Character, String> dict, Set<String> used) {
-        if (pattern.length() == 0) {
-            return str.length() == 0;
-        }
+        if (pattern.length() == 0) return str.length() == 0;
         Character ch = pattern.charAt(0);
         if (dict.containsKey(ch)) {
             String word = dict.get(ch);
-            if (!str.startsWith(word))  return false;
+            if (!str.startsWith(word)) return false;
             return match(pattern.substring(1), str.substring(dict.get(ch).length()), dict, used);
         }
         for (int i = 0; i < str.length(); i++) {
@@ -25,7 +25,7 @@ public class WordPatternII {
             dict.put(ch, word);
             used.add(word);
 
-            if (match(pattern.substring(1), str.substring(i + 1), dict, used))  return true;
+            if (match(pattern.substring(1), str.substring(i + 1), dict, used)) return true;
 
             used.remove(word);
             dict.remove(ch);
