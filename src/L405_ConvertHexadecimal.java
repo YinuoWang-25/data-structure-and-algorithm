983c308125264a3ed8e58356df3e928e7659a204
YinuoWang-25 <yinuowang0825@gmail.com>
2020-09-24 21:24:00 -0700

    405. Convert a Number to Hexadecimal

--- src/L405_ConvertHexadecimal.java
+++ src/L405_ConvertHexadecimal.java
@@ -1,10 +1,12 @@
-package todo;
+// 405. Convert a Number to Hexadecimal
+/*
+num & 15 to get the num we need to insert at head
+ */
 
-public class ConvertNumberHexadecimal {
-    // 405. Convert a Number to Hexadecimal
+public class L405_ConvertHexadecimal {
     public String toHex(int num) {
         if (num == 0) return "0";
-        char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        char[] map = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         String result = "";
         while (num != 0) {
             result = map[(num & 15)] + result;
