bfc4171df6bd33445ac5f1c604b24cead447c846
YinuoWang-25 <yinuowang0825@gmail.com>
2020-10-03 20:37:57 -0700

    update basic calculator

--- src/L224_BasicCalculator.java
+++ src/L224_BasicCalculator.java
@@ -5,38 +5,32 @@ import java.util.LinkedList;
 
 public class L224_BasicCalculator {
     public int calculate(String s) {
-        Deque<Integer> preStack = new LinkedList<>();
-        Deque<Integer> signStack = new LinkedList<>();
-        int pre = 0;
-        int sign = 1;
-        int num = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '+') {
-                pre += sign * num;
+        Deque<Integer> nums = new LinkedList<>(), signs = new LinkedList<>();
+        int res = 0, sign = 1, num = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                nums.offerFirst(res);
+                signs.offerFirst(sign);
+                sign = 1;
+                res = 0;
+            } else if (c == ')') {
+                res += num * sign;
+                res = nums.pollFirst() + signs.pollFirst() * res;
+                sign = 1;
+                num = 0;
+            } else if (c == '+') {
+                res += sign * num;
                 num = 0;
                 sign = 1;
             } else if (c == '-') {
-                pre += sign * num;
+                res += sign * num;
                 num = 0;
                 sign = -1;
             } else if (Character.isDigit(c)) {
                 num = num * 10 + (c - '0');
-            } else if (c == '(') {
-                preStack.offerFirst(pre);
-                signStack.offerFirst(sign);
-                pre = 0;
-                sign = 1;
-            } else if (c == ')') {
-                pre += sign * num;
-                pre = preStack.pollFirst() + signStack.pollFirst() * pre;
-                sign = 1;
-                num = 0;
             }
         }
-        if (num > 0) {
-            pre += num * sign;
-        }
-        return pre;
+        if (num > 0) res += num * sign;
+        return res;
     }
 }
