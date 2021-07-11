41de18a948ae7143e625ee55db08a27b0e02d52b
YinuoWang-25 <71306986+YinuoWang-25@users.noreply.github.com>
2021-07-10 18:34:45 -0700

    update range sum

--- src/L938_RangeSum.java
+++ src/L938_RangeSum.java
@@ -5,15 +5,19 @@ import common.TreeNode;
 public class L938_RangeSum {
 
     public int rangeSumBST(TreeNode root, int L, int R) {
-        int[] sum = new int[1];
-        getSum(root, L, R, sum);
-        return sum[0];
-    }
+        if (root == null) {
+            return 0;
+        }
 
-    private void getSum(TreeNode root, int L, int R, int[] sum) {
-        if (root == null) return;
-        if (root.val >= L && root.val <= R) sum[0] += root.val;
-        if (root.val > L) getSum(root.left, L, R, sum);
-        if (root.val < R) getSum(root.right, L, R, sum);
+        int res = 0;
+
+        if (root.val >= L && root.val <= R) {
+            res += root.val;
+        }
+
+        if (root.val > L) res += rangeSumBST(root.left, L, R);
+        if (root.val < R) res += rangeSumBST(root.right, L, R);
+
+        return res;
     }
 }
