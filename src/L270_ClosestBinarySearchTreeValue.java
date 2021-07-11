4e41a8efd4555b2e95ff3d750d366a28496b54aa
YinuoWang-25 <71306986+YinuoWang-25@users.noreply.github.com>
2021-07-10 18:42:05 -0700

    update 270

--- src/L270_ClosestBinarySearchTreeValue.java
+++ src/L270_ClosestBinarySearchTreeValue.java
@@ -4,15 +4,18 @@ import common.TreeNode;
 
 public class L270_ClosestBinarySearchTreeValue {
     public int closestValue(TreeNode root, double target) {
-        int ans = root.val;
-        while (root != null) {
-            ans = Math.abs(root.val - target) < Math.abs(ans - target) ? root.val : ans;
-            if (root.val < target) {
-                root = root.right;
-            } else {
-                root = root.left;
-            }
+        if (root == null) {
+            return -1;
         }
-        return ans;
+
+        if (root.val < target) {
+            int r = closestValue(root.right, target);
+            return Math.abs(r - target) < Math.abs(root.val - target) ? r : root.val;
+        } else if (root.val > target) {
+            int l = closestValue(root.left, target);
+            return Math.abs(l - target) < Math.abs(root.val - target) ? l : root.val;
+        }
+
+        return root.val;
     }
 }
