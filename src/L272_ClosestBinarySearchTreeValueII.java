99d9837fe27e0288dcf60f5e8f2a899d9f5a339b
YinuoWang-25 <71306986+YinuoWang-25@users.noreply.github.com>
2021-07-10 23:34:51 -0700

    solution 2 for 272

--- src/L272_ClosestBinarySearchTreeValueII.java
+++ src/L272_ClosestBinarySearchTreeValueII.java
@@ -8,6 +8,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class L272_ClosestBinarySearchTreeValueII {
+
+    /*
+
+    Recursive
+
+     */
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         Deque<Integer> res = new LinkedList<>();
         inorder(root, target, k, res);
@@ -18,7 +24,9 @@ public class L272_ClosestBinarySearchTreeValueII {
         if (root == null) {
             return;
         }
+
         inorder(root.left, target, k, res);
+
         if (res.size() == k) {
             if (Math.abs(root.val - target) > Math.abs(res.peekFirst() - target)) {
                 return;
@@ -26,7 +34,55 @@ public class L272_ClosestBinarySearchTreeValueII {
                 res.pollFirst();
             }
         }
+
         res.offerLast(root.val);
+
         inorder(root.right, target, k, res);
+    }
+
+
+    /*
+
+    Iterative
+
+     */
+
+    public List<Integer> closestKValuesII(TreeNode root, double target, int k) {
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Deque<Integer> queue = new LinkedList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+
+
+        pushAll(stack, root);
+
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pollFirst();
+
+            if (queue.size() == k) {
+                if (Math.abs(target - queue.peekFirst()) <= Math.abs(target - cur.val)) {
+                    break;
+                } else {
+                    queue.pollFirst();
+                }
+            }
+
+            queue.offerLast(cur.val);
+            pushAll(stack, cur.right);
+        }
+
+        return new ArrayList<>(queue);
+
+    }
+
+    private void pushAll(Deque<TreeNode> stack, TreeNode root) {
+        while (root != null) {
+            stack.offerFirst(root);
+            root = root.left;
+        }
     }
 }
