c16b73d154e9b5c51267a891419bc04456e965ec
YinuoWang-25 <71306986+YinuoWang-25@users.noreply.github.com>
2021-07-11 10:10:22 -0700

    Update L114_FlattenBinaryTreeLinkedList.java

--- src/L114_FlattenBinaryTreeLinkedList.java
+++ src/L114_FlattenBinaryTreeLinkedList.java
@@ -4,26 +4,29 @@ import common.TreeNode;
 
 public class L114_FlattenBinaryTreeLinkedList {
     public void flatten(TreeNode root) {
-        dfs(root);
+        traverse(root);
     }
 
-    private TreeNode dfs(TreeNode root) {
+    private TreeNode traverse(TreeNode root) {
         if (root == null) {
             return null;
         }
-        TreeNode right = root.right;
-        TreeNode rightMost = root.left;
-        if (rightMost == null) {
-            root.right = dfs(root.right);
-        } else {
-            while (rightMost.right != null) {
-                rightMost = rightMost.right;
-            }
-            rightMost.right = right;
-            root.right = dfs(root.left);
-            root.left = null;
-        }
-        return root;
 
+        TreeNode l = traverse(root.left);
+        TreeNode r = traverse(root.right);
+
+        root.left = null;
+
+        if (l == null) {
+            root.right = r;
+        } else {
+            root.right = l;
+            while (l.right != null) {
+                l = l.right;
+            }
+            l.right = r;
+        }
+
+        return root;
     }
 }
